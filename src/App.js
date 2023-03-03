4dbda6ae816a9696471ee2a28dd7042a7750749f
Asante <asante0277@gmail.com>
2023-03-02 21:43:55 -0800

    Working on App

--- src/App.js
+++ src/App.js
@@ -3,9 +3,15 @@ import Task from './Task';
 import TaskForm from './TaskForm';
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+  function addTask(name) {
+     setTasks(prev => {
+        return [...prev, {name:name, done:false}];
+     })
+  }
   return (
     <main>
-      <TaskForm />
+      <TaskForm onAdd={addTask} />
       <Task />
     </main>
   );
