feefdc9c9c06966952131998d10154f380b82289
Asante <asante0277@gmail.com>
2023-03-03 16:06:32 -0800

    Conponents contents updated

--- src/App.js
+++ src/App.js
@@ -1,20 +1,40 @@
 import './App.css';
 import Task from './Task';
 import TaskForm from './TaskForm';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
   const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    if (tasks.length === 0) return;
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }, [tasks]);
+
+  useEffect(() => {
+    const tasks = JSON.parse(localStorage.getItem('tasks'));
+    setTasks(tasks);
+  }, []);
+
   function addTask(name) {
+    setTasks(prev => {
+      return [...prev, { name: name, done: false }];
+    });
+  }
+
+  function updateTaskDone(taskIndex, newDone) {
      setTasks(prev => {
-        return [...prev, {name:name, done:false}];
+        const newTasks = [...prev];
+        newTasks[taskIndex].done = newDone;
+        return newTasks;
      });
   }
+
   return (
     <main>
       <TaskForm onAdd={addTask} />
-      {tasks.map(task => (
-         <Task {...task} />
+      {tasks.map((task, index) => (
+        <Task {...task} onToggle={done => updateTaskDone(index, done)} />
       ))}
     </main>
   );
